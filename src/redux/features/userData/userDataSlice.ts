38810d1af4de90a03f3c7d44fa4231bd5eb5ee7b
Moshe Winberg <59839782+MosheRW@users.noreply.github.com>
2025-01-12 12:28:38 +0200

    ✨ feat(UserDate - Redux):
    all local storage writing will made from inside the redux

---
+++ src/redux/features/userData/userDataSlice.ts
@@ -0,0 +1,55 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { User } from "../../../types/user/user";
+
+interface UserState extends User {
+    token: string;
+}
+
+
+const initialState: UserState = {
+    id: "",
+    userName: "",
+    fullName: "",
+    email: "",
+    password: "",
+    created_at: new Date(),
+    token: "",
+};
+
+export const userDataSlice = createSlice({
+    name: "userData",
+    initialState,
+    reducers: {
+        setUserID: (state, action: PayloadAction<string>) => {
+            state.id = action.payload;
+            localStorage.setItem("userId", state.id);
+        },
+        setUserData: (state, action: PayloadAction<UserState>) => {
+            state.id = action.payload.id;
+            localStorage.setItem("userId", state.id);
+            
+            state.userName = action.payload.userName;
+            localStorage.setItem("userName", state.userName);
+
+            state.fullName = action.payload.fullName;
+            localStorage.setItem("userFullName", state.fullName);
+
+            state.email = action.payload.email;
+            localStorage.setItem("userEmail", state.email);
+
+            state.password = action.payload.password;
+            // it is safe and/or necessary?
+            localStorage.setItem("userPassword", state.password); 
+            
+            state.created_at = action.payload.created_at;
+            localStorage.setItem("userCreated_at", state.created_at.toUTCString()); 
+
+            state.token = action.payload.token;
+            localStorage.setItem("userToken", state.token); 
+
+        },
+    },
+});
+
+export const { setUserData } = userDataSlice.actions;
+export default userDataSlice.reducer;
