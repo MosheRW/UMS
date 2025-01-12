150cf042533f348be9085fdb7aeffa5322271b7d
Moshe Winberg <59839782+MosheRW@users.noreply.github.com>
2025-01-12 12:27:12 +0200

    ✨ feat(redux): start redux

---
+++ src/redux/rootReducer.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import userData from './features/userData/userDataSlice';
+
+const rootReducer = combineReducers({
+  userData: userData
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+export default rootReducer;
---
+++ src/redux/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import rootReducer from "./rootReducer";
+const loggerMiddleware = logger;
+
+// if the env file includes REACT_APP_REDUX=LOGS, it will display the logs
+
+const store = (process.env.REACT_APP_REDUX == "LOGS") ? configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(loggerMiddleware),
+}) : configureStore({
+  reducer: rootReducer,
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
