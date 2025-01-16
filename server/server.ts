d99c74612698540d59d82313cba0d64e85c3fb52
Moshe Winberg <59839782+MosheRW@users.noreply.github.com>
2025-01-16 11:45:52 +0200

    2

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,14 @@
 import express from 'express';
 import path from 'path';
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
