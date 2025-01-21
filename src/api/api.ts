5f432288112a87abc31c926bf59c8fe691a77026
Moshe Winberg (TheRedOne) <MosheWinberg@gmail.com>
2025-01-21 23:08:47 +0200

    🐞 fix(api.ts): fixed

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,6 @@
 import { urls } from './urls';
 import * as services from './services';
+import { User } from '../types/user/user';
 
 export interface LoginUser {
   username: string;
@@ -12,11 +13,11 @@ export const api = {
     services.get({ url: urls.getAllUsers(), isToast: toast }),
   getAUser: async (userID: string, toast: boolean = false) =>
     services.get({ url: urls.getAUser(userID), isToast: toast }),
-  postAnewUser: async (user: any, toast: boolean = false) =>
+  postAnewUser: async (user: Omit<User, 'id'>, toast: boolean = false) =>
     services.post({ url: urls.postAnewUser(), body: user, isToast: toast }),
-  updateAUser: async (userID: string, user: any, toast: boolean = false) =>
+  updateAUser: async (userID: string, user: Partial<User>, toast: boolean = false) =>
     services.put({ url: urls.updateAUser(userID), body: user, isToast: toast }),
-  signUpUser: async (user: any, token?: string, toast: boolean = false) =>
+  signUpUser: async (user:  Omit<User, 'id'>, token?: string, toast: boolean = false) =>
     services.post({ url: urls.postAnewUser(), body: user, isToast: toast }),
   deleteAUser: async (userID: string, toast: boolean = false) =>
     services.del({ url: urls.deleteAUser(userID), isToast: toast }),
